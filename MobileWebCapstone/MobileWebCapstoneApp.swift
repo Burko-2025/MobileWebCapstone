6838961b1fa8719406856bae6fd3408811288d10
kyleburk <kyle.burk@triosstudent.com>
2026-05-29 08:43:38 -0400

    Initial Commit

---
+++ MobileWebCapstone/MobileWebCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileWebCapstoneApp.swift
+//  MobileWebCapstone
+//
+//  Created by Kyle Burk on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct MobileWebCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MobileWebCapstoneTests/MobileWebCapstoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileWebCapstoneTests.swift
+//  MobileWebCapstoneTests
+//
+//  Created by Kyle Burk on 2026-05-29.
+//
+
+import Testing
+@testable import MobileWebCapstone
+
+struct MobileWebCapstoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
